2a33c0268ba4ad7c801b3546bbf002a5651ce33c
Mael29000 <maelsuard@orange.fr>
2025-06-06 17:09:18 +0200

    Initial Commit

---
+++ LaVideoLaPlusVue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaVideoLaPlusVue
+//
+//  Created by Maël Suard on 06/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaVideoLaPlusVue/LaVideoLaPlusVueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaVideoLaPlusVueApp.swift
+//  LaVideoLaPlusVue
+//
+//  Created by Maël Suard on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct LaVideoLaPlusVueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
